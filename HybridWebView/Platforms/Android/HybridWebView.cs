95700896cb2d5717b03353275ba9bc184b3578bb
Eilon Lipton <Eilon@users.noreply.github.com>
2022-12-08 11:56:28 -0800

    Add basic Android message functionality

--- HybridWebView/Platforms/Android/HybridWebView.cs
+++ HybridWebView/Platforms/Android/HybridWebView.cs
@@ -1,12 +1,42 @@
-﻿using AWebView = Android.Webkit.WebView;
+﻿using Android.Webkit;
+using Java.Interop;
+using AWebView = Android.Webkit.WebView;
 
 namespace HybridWebView
 {
     partial class HybridWebView
     {
-        partial void InitializeHybridWebView(string mainFileAssetPath)
+        private HybridWebViewJavaScriptInterface _javaScriptInterface;
+        async partial void InitializeHybridWebView(string mainFileAssetPath)
         {
             var awv = (AWebView)Handler.PlatformView;
+            awv.Settings.JavaScriptEnabled = true;
+
+            _javaScriptInterface = new HybridWebViewJavaScriptInterface(this);
+            awv.AddJavascriptInterface(_javaScriptInterface, "hybridWebViewHost");
+            awv.LoadDataWithBaseURL(
+                baseUrl: "https://0.0.0.0/",
+                data: await GetAssetContentAsync(mainFileAssetPath),
+                mimeType: "text/html",
+                encoding: "UTF-8",
+                historyUrl: null);
+        }
+
+        private sealed class HybridWebViewJavaScriptInterface : Java.Lang.Object
+        {
+            private readonly HybridWebView _hybridWebView;
+
+            public HybridWebViewJavaScriptInterface(HybridWebView hybridWebView)
+            {
+                _hybridWebView = hybridWebView;
+            }
+
+            [JavascriptInterface]
+            [Export("sendMessage")]
+            public void SendMessage(string message)
+            {
+                _hybridWebView.OnMessageReceived(message);
+            }
         }
     }
 }
