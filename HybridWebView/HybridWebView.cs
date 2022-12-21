964325132410fdf427f7c2ac63f3a5e3c3f0c818
Eilon Lipton <Eilon@users.noreply.github.com>
2022-12-21 13:57:55 -0800

    Add InvokeJsMethodAsync() API to serialize parameters for JavaScript calls
    And optionally deserialize return values.

--- HybridWebView/HybridWebView.cs
+++ HybridWebView/HybridWebView.cs
@@ -1,4 +1,6 @@
-﻿namespace HybridWebView
+﻿using System.Text.Json;
+
+namespace HybridWebView
 {
     public partial class HybridWebView : WebView
     {
@@ -15,6 +17,38 @@
         }
 
         partial void InitializeHybridWebView();
+
+        /// <summary>
+        /// Invokes a JavaScript method named <paramref name="methodName"/> and optionally passes in the parameter values specified
+        /// by <paramref name="paramValues"/> by JSON-encoding each one.
+        /// </summary>
+        /// <param name="methodName">The name of the JavaScript method to invoke.</param>
+        /// <param name="paramValues">Optional array of objects to be passed to the JavaScript method by JSON-encoding each one.</param>
+        /// <returns>A string containing the return value of the called method.</returns>
+        public async Task<string> InvokeJsMethodAsync(string methodName, params object[] paramValues)
+        {
+            if (string.IsNullOrEmpty(methodName))
+            {
+                throw new ArgumentException($"The method name cannot be null or empty.", nameof(methodName));
+            }
+
+            return await EvaluateJavaScriptAsync($"{methodName}({(paramValues == null ? string.Empty : string.Join(", ", paramValues.Select(v => JsonSerializer.Serialize(v))))})");
+        }
+
+        /// <summary>
+        /// Invokes a JavaScript method named <paramref name="methodName"/> and optionally passes in the parameter values specified
+        /// by <paramref name="paramValues"/> by JSON-encoding each one.
+        /// </summary>
+        /// <typeparam name="TReturnType">The type of the return value to deserialize from JSON.</typeparam>
+        /// <param name="methodName">The name of the JavaScript method to invoke.</param>
+        /// <param name="paramValues">Optional array of objects to be passed to the JavaScript method by JSON-encoding each one.</param>
+        /// <returns>An object of type <typeparamref name="TReturnType"/> containing the return value of the called method.</returns>
+        public async Task<TReturnType> InvokeJsMethodAsync<TReturnType>(string methodName, params object[] paramValues)
+        {
+            var stringResult = await InvokeJsMethodAsync(methodName, paramValues);
+
+            return JsonSerializer.Deserialize<TReturnType>(stringResult);
+        }
 
         public virtual void OnMessageReceived(string message)
         {
--- MauiCSharpInteropWebView/MainPage.xaml.cs
+++ MauiCSharpInteropWebView/MainPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OnCounterClicked(object sender, EventArgs e)
     {
         count++;
 
@@ -23,7 +23,10 @@ public partial class MainPage : ContentPage
 
         SemanticScreenReader.Announce(CounterBtn.Text);
 
-        _ = webView.EvaluateJavaScriptAsync($"SendToJs('hi from .net, counter={count}!')");
+        _ = await webView.EvaluateJavaScriptAsync($"SendToJs('hi from .net, counter={count}!')");
+        _ = await webView.InvokeJsMethodAsync("SendToJsWithArgs", 123.456789, "Nice!", DateTimeOffset.Now);
+        var sum = await webView.InvokeJsMethodAsync<int>("SendToJsAddNumbers", 123, 456);
+        Debug.WriteLine($"JS Return value received with sum: {sum}");
     }
 
     private void webView_MessageReceived(object sender, HybridWebView.HybridWebViewMessageReceivedEventArgs e)
