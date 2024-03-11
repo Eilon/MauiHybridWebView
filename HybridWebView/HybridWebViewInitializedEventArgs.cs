0e2565eace63b913494c8bd13ccbb6978d5aee93
Eilon Lipton <Eilon@users.noreply.github.com>
2024-03-11 16:18:08 -0700

    Expose platform webview for initialization to enable customization
    Fixes #45

---
+++ HybridWebView/HybridWebViewInitializedEventArgs.cs
@@ -0,0 +1,43 @@
+﻿#if WINDOWS
+using Microsoft.Web.WebView2.Core;
+using WebView2Control = Microsoft.UI.Xaml.Controls.WebView2;
+#elif ANDROID
+using AWebView = Android.Webkit.WebView;
+#elif IOS || MACCATALYST
+using WebKit;
+#elif TIZEN
+using TWebView = Tizen.WebView.WebView;
+#endif
+
+namespace HybridWebView
+{
+    /// <summary>
+    /// Allows configuring the underlying web view after it has been initialized.
+    /// </summary>
+    public class HybridWebViewInitializedEventArgs : EventArgs
+    {
+#nullable disable
+#if WINDOWS
+		/// <summary>
+		/// Gets the <see cref="WebView2Control"/> instance that was initialized.
+		/// </summary>
+		public WebView2Control WebView { get; internal set; }
+#elif ANDROID
+		/// <summary>
+		/// Gets the <see cref="AWebView"/> instance that was initialized.
+		/// </summary>
+		public AWebView WebView { get; internal set; }
+#elif MACCATALYST || IOS
+		/// <summary>
+		/// Gets the <see cref="WKWebView"/> instance that was initialized.
+		/// the default values to allow further configuring additional options.
+		/// </summary>
+		public WKWebView WebView { get; internal set; }
+#elif TIZEN
+		/// <summary>
+		/// Gets the <see cref="TWebView"/> instance that was initialized.
+		/// </summary>
+		public TWebView WebView { get; internal set; }
+#endif
+    }
+}
