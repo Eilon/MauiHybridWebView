d94b4968cdd9808ee51ccf0f8a55149ddbd22245
Leonardo R. da Silva <leonardo.silva@digitalpages.com.br>
2024-05-22 16:52:53 -0300

    Adds the ability to direct different routes to the proxy based on prefix

--- HybridWebView/Platforms/MacCatalyst/HybridWebViewHandler.MacCatalyst.cs
+++ HybridWebView/Platforms/MacCatalyst/HybridWebViewHandler.MacCatalyst.cs
@@ -127,7 +127,7 @@ namespace HybridWebView
                     Stream? contentStream = null;
 
                     // Check to see if the request is a proxy request.
-                    if (relativePath == HybridWebView.ProxyRequestPath)
+                    if (relativePath == HybridWebView.ProxyRequestPath || relativePath?.StartsWith($"{HybridWebView.ProxyRequestPath}\\") == true)
                     {
                         var args = new HybridWebViewProxyEventArgs(fullUrl);
 
